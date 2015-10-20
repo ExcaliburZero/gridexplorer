cbb3e83700a8c4ad3f5435156ea1a5d74fc3025c
ExcaliburZero <cwellsny@nycap.rr.com>
2015-10-20 15:06:47 -0400

    Added more tests of the Room class methods
    Added tests for the display and formatPosition methods of the Room class. Also added streams to use to test the output given through print statements.
    
    Setup a better method to handle assert test descriptions.

--- test/gridexplorer/RoomTest.java
+++ test/gridexplorer/RoomTest.java
@@ -23,6 +23,8 @@
  */
 package gridexplorer;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -38,7 +40,13 @@ public class RoomTest {
 
 	private String[] testRooms = {"room1"};
 	private Game testGame = new Game(testRooms);
-	private Room testRoom = new Room(4, 5, 3, 2, testGame);
+	private int testRows = 4;
+	private int testColumns = 5;
+	private int testSpawnRow = 3;
+	private int testSpawnColumn = 2;
+	private Room testRoom = new Room(testRows, testColumns, testSpawnRow, testSpawnColumn, testGame);
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
 	public RoomTest() {
 	}
@@ -53,29 +61,71 @@ public class RoomTest {
 
 	@Before
 	public void setUp() {
+		// Setup output streams
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
 	}
 
 	@After
 	public void tearDown() {
+		// Cleanup output streams
+		System.setOut(null);
+		System.setErr(null);
 	}
-	
+
 	/**
 	 * A test which checks to make sure that the getColumns method of the Room
 	 * class returns the correct value for a basic room.
 	 */
 	@Test
 	public void testGetColumns() {
+		String testInfo = "A test for the getColumns method of Room class";
 		int columns = testRoom.getColumns();
-		assertEquals("A test for the getColumns method of Room class", 5, columns);
+		assertEquals(testInfo, testColumns, columns);
 	}
-	
+
 	/**
 	 * A test which checks to make sure that the getRows method of the Room
 	 * class returns the correct value for a basic room.
 	 */
 	@Test
 	public void testGetRows() {
+		String testInfo = "A test for the getRows method of Room class";
 		int rows = testRoom.getRows();
-		assertEquals("A test for the getRows method of Room class", 4, rows);
+		assertEquals(testInfo, testRows, rows);
+	}
+
+	/**
+	 * A test which attempts to display a basic room and checks that the output
+	 * is correct for the given info about the room.
+	 */
+	@Test
+	public void testDisplay() {
+		String testInfo = "A test for the creation and display of a basic room";
+		testRoom.display();
+		assertEquals(testInfo, "     \n     \n     \n  @  \n", outContent.toString());
+	}
+
+	/**
+	 * A test which checks the two integer argument form of the formatPosition
+	 * method of the Room class.
+	 */
+	@Test
+	public void testFormatPositionInts() {
+		String testInfo = "A test for the formatPosition method of Room class";
+		String returnedString = testRoom.formatPosition(1, 2);
+		assertEquals(testInfo, "(1, 2)", returnedString);
+	}
+
+	/**
+	 * A test which checks the integer array argument form of the formatPosition
+	 * method of the Room class.
+	 */
+	@Test
+	public void testFormatPositionIntArray() {
+		String testInfo = "A test for the formatPosition method of Room class";
+		int[] testPoints = {1, 2};
+		String returnedString = testRoom.formatPosition(testPoints);
+		assertEquals(testInfo, "(1, 2)", returnedString);
 	}
 }
